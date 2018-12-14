265244e9ae1bcd737d07eb81cdd76cadfe41e8d3
priyankaBitcanny <priyanka@bitcanny.com>
2018-12-15 00:49:50 +0530

    add new items

--- ToDoay/ToDoViewController.swift
+++ ToDoay/ToDoViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ToDoViewController: UITableViewController {
-    var itemArray: [String] = ["gdgf", "fyjhg", "sertyuio", "ertyui"]
+    var itemArray: [String] = [String]()
     var selectedType = ""
 
     override func viewDidLoad() {
@@ -39,6 +39,26 @@ class ToDoViewController: UITableViewController {
         return itemArray.count
     }
 
+    //MARK:- Add new item to list
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print(textField.text ?? "")
+            if let text = textField.text {
+                self.itemArray.append(text)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
